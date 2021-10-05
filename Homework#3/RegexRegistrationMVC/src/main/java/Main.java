91a136b63f27e36bd43b37a65c957f61c50f9f63
Yezhyck <sergeysergeevytch@gmail.com>
2021-10-05 16:05:13 +0300

    Private Scanner field

--- Homework#3/RegexRegistrationMVC/src/main/java/Main.java
+++ Homework#3/RegexRegistrationMVC/src/main/java/Main.java
@@ -1,10 +1,11 @@
 import controller.Controller;
+import model.NoteBook;
 import model.NoteBookRecord;
 import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        Controller Controller = new Controller(new NoteBookRecord(), new View());
+        Controller Controller = new Controller(new NoteBookRecord(), new NoteBook(), new View());
         Controller.inputNote();
     }
 }
