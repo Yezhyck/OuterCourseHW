405ac3cf155188507d323fc61f98293934230aa3
Yezhyck <sergeysergeevytch@gmail.com>
2021-10-06 09:50:55 +0300

    New Controller constructor

--- Homework#3/RegexRegistrationMVC/src/main/java/Main.java
+++ Homework#3/RegexRegistrationMVC/src/main/java/Main.java
@@ -1,11 +1,10 @@
 import controller.Controller;
 import model.NoteBook;
-import model.NoteBookRecord;
 import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        Controller Controller = new Controller(new NoteBookRecord(), new NoteBook(), new View());
+        Controller Controller = new Controller(new NoteBook(), new View());
         Controller.inputNote();
     }
 }
